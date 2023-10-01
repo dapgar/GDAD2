5de929d327fed884ced9d657ab7f8f820ab1903b
Dominic Apgar <dominicapgar2100@gmail.com>
2023-10-01 15:59:49 -0400

    Polished Turn-based Combat
    Finished combat manager and made it triggerable.

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -2,24 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor.VersionControl;
 
-public enum BattleState { START, PLAYERTURN, ENEMYTURN, WIN, LOSE }
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WIN, LOSE, OUTCOME }
 
 public class CombatManager : MonoBehaviour
 {
     public GameObject combatScreen;
-    public GameObject[] buttons;
 
+    [Header("Characters")]
     public GameObject enemy;
     public GameObject player;
 
     private EnemyStatus enemyStatus;
     private PlayerStatus playerStatus;
 
+    [Header("UI Elements")]
     public TextMeshProUGUI playerHealth;
     public TextMeshProUGUI enemyHealth;
     public TextMeshProUGUI turnIndicator;
     public TextMeshProUGUI battleOutcome;
+
+    public GameObject[] combatIcons;
+    public GameObject[] combatButtons;
 
     private int playerChoice;
     private int enemyChoice;
@@ -33,8 +38,17 @@ public class CombatManager : MonoBehaviour
     {
         enemyStatus = enemy.GetComponent<EnemyStatus>();
         playerStatus = player.GetComponent<PlayerStatus>();
+    }
 
+    // Use this method to trigger the combat sequence.
+    public void StartCombat()
+    {
         combatScreen.SetActive(true);
+        foreach (GameObject button in combatButtons)
+        {
+            button.SetActive(false);
+        }
+
         battleState = BattleState.START;
         StartCoroutine(BeginBattle());
     }
@@ -49,7 +63,7 @@ public class CombatManager : MonoBehaviour
         yield return new WaitForSeconds(1f);
 
         // Fade in character sprites
-       
+
         // Player's turn
         battleState = BattleState.PLAYERTURN;
         yield return StartCoroutine(PlayerTurn());
@@ -57,40 +71,29 @@ public class CombatManager : MonoBehaviour
 
     IEnumerator PlayerTurn()
     {
-        // Display message
-        Debug.Log("Mouse Unlocked");
-        yield return new WaitForSeconds(1);
-
         // release the blockade on clicking 
-        // so that player can click on 'attack' button    
+        // so that player can click on 'attack' button
+        foreach (GameObject button in combatButtons)
+        {
+            button.SetActive(true);
+        }
         hasClicked = false;
+        yield return null;
     }
 
     IEnumerator EnemyTurn()
     {
+        foreach (GameObject button in combatButtons)
+        {
+            button.SetActive(false);
+        }
+
         yield return new WaitForSeconds(2);
         enemyChoice = Random.Range(1, 3);
+        combatIcons[enemyChoice + 2].SetActive(true);
 
+        battleState = BattleState.OUTCOME;
         StartCoroutine(CombatOutcome());
-
-        /*
-        // decrease playerhealth by a fixed amount of 10.
-        playerStatus.TakeDamage(10);
-
-        if (playerStatus.currentHealth <= 0)
-        {
-            // if the player health drops to 0 the enemy wins.
-            battleState = BattleState.LOSE;
-            yield return StartCoroutine(EndBattle());
-        }
-        else
-        {
-            // if the player health is still above 0 
-            // when the turn finishes it's player's turn
-            battleState = BattleState.PLAYERTURN;
-            yield return StartCoroutine(PlayerTurn());
-        }
-        */
     }
 
     IEnumerator EndBattle()
@@ -122,6 +125,7 @@ public class CombatManager : MonoBehaviour
         if (!hasClicked)
         {
             playerChoice = 1;
+            combatIcons[0].SetActive(true);
             // block user from repeatedly pressing attack button  
             hasClicked = true;
 
@@ -140,6 +144,7 @@ public class CombatManager : MonoBehaviour
         if (!hasClicked)
         {
             playerChoice = 2;
+            combatIcons[1].SetActive(true);
             // block user from repeatedly pressing attack button  
             hasClicked = true;
 
@@ -158,6 +163,7 @@ public class CombatManager : MonoBehaviour
         if (!hasClicked)
         {
             playerChoice = 3;
+            combatIcons[2].SetActive(true);
             // block user from repeatedly pressing attack button  
             hasClicked = true;
 
@@ -169,7 +175,7 @@ public class CombatManager : MonoBehaviour
     IEnumerator CombatOutcome()
     {
         // Battle Logic
-    #region
+        #region
         if (playerChoice == 1 && enemyChoice == 1)
         {
             // Both atk
@@ -182,48 +188,56 @@ public class CombatManager : MonoBehaviour
             // Player def, enemy atk
             battleOutcome.text = "Player Blocked Attack!";
         }
-        else if(playerChoice == 3 && enemyChoice == 1)
+        else if (playerChoice == 3 && enemyChoice == 1)
         {
             // Player mgk, enemy atk
-            playerStatus.TakeDamage(10f);
-            battleOutcome.text = "Enemy Countered Magic!";
+            playerStatus.TakeDamage(30f);
+            battleOutcome.text = "Enemy Punished Magic!";
         }
-        else if(playerChoice == 1 && enemyChoice == 2)
+        else if (playerChoice == 1 && enemyChoice == 2)
         {
             // Player atk, enemy def
             battleOutcome.text = "Enemy Blocked Attack!";
         }
-        else if(playerChoice == 2 && enemyChoice == 2)
+        else if (playerChoice == 2 && enemyChoice == 2)
         {
             // Both def
             battleOutcome.text = "Both Blocked!";
         }
-        else if(playerChoice == 3 && enemyChoice == 2)
+        else if (playerChoice == 3 && enemyChoice == 2)
         {
             // Player mgk, enemy def
-            enemyStatus.TakeDamage(30f);
-            battleOutcome.text = "Player Countered Block!";
+            enemyStatus.TakeDamage(20f);
+            battleOutcome.text = "Player Punished Block!";
         }
-        else if(playerChoice == 1 && enemyChoice == 3)
+        else if (playerChoice == 1 && enemyChoice == 3)
         {
             // Player atk, enemy mgk
-            enemyStatus.TakeDamage(10f);
-            battleOutcome.text = "Player Countered Magic!";
+            enemyStatus.TakeDamage(30f);
+            battleOutcome.text = "Player Punished Magic!";
         }
-        else if(playerChoice == 2 && enemyChoice == 3)
+        else if (playerChoice == 2 && enemyChoice == 3)
         {
             // Player def, enemy mgk
-            playerStatus.TakeDamage(30f);
-            battleOutcome.text = "Enemy Countered Block!";
+            playerStatus.TakeDamage(20f);
+            battleOutcome.text = "Enemy Punished Block!";
         }
-        else if(playerChoice == 3 && enemyChoice == 3)
+        else if (playerChoice == 3 && enemyChoice == 3)
         {
             // Both mgk
-            playerStatus.TakeDamage(30f);
-            enemyStatus.TakeDamage(30f);
+            playerStatus.TakeDamage(20f);
+            enemyStatus.TakeDamage(20f);
             battleOutcome.text = "Both Used Magic!";
         }
         #endregion
+
+        yield return new WaitForSeconds(2f);
+
+        battleOutcome.text = " ";
+        foreach (GameObject icon in combatIcons)
+        {
+            icon.SetActive(false);
+        }
 
         if (enemyStatus.currentHealth <= 0)
         {
@@ -246,33 +260,34 @@ public class CombatManager : MonoBehaviour
         }
     }
 
-    IEnumerator PlayerAttack()
-    {
-        // decrease enemy health by a fixed amount of 10
-        enemyStatus.TakeDamage(10);
-
-        if (enemyStatus.currentHealth <= 0)
-        {
-            // if the enemy health drops to 0 player wins.
-            battleState = BattleState.WIN;
-            yield return StartCoroutine(EndBattle());
-        }
-        else
-        {
-            // if the enemy health is still above 0 
-            // when the turn finishes it's enemy's turn.
-            battleState = BattleState.ENEMYTURN;
-            yield return StartCoroutine(EnemyTurn());
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
         playerHealth.text = playerStatus.currentHealth + "/" + playerStatus.maxHealth;
         enemyHealth.text = enemyStatus.currentHealth + "/" + enemyStatus.maxHealth;
 
-        turnIndicator.text = battleState.ToString();
+        switch(battleState)
+        {
+            case BattleState.PLAYERTURN:
+                turnIndicator.text = "Your Turn";
+                break;
 
+            case BattleState.ENEMYTURN:
+                turnIndicator.text = "Enemy's Turn";
+                break;
+
+            case BattleState.WIN:
+                turnIndicator.text = "Enemy Slain";
+                break;
+
+            case BattleState.LOSE:
+                turnIndicator.text = "You've Died";
+                break;
+
+            case BattleState.OUTCOME:
+            case BattleState.START:
+                turnIndicator.text = " ";
+                break;
+        }
     }
 }
