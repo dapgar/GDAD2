3c1334cd6346d862bd11f41e25867e5384ad2e93
Esther Loo <12ekloo02@gmail.com>
2023-10-05 20:18:15 -0400

    pause menu and main menu working
    pause menu and main menu work, but look super ugly

---
+++ Assets/Scripts/TempUI/TempPause.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class TempPause : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void TogglePause(InputAction.CallbackContext context)
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Home(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+}
